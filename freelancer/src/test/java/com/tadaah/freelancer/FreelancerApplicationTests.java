486cdac09a4dcb6473bca9398b014b425c443ded
amohan751 <abhijith.or.mohan@gmail.com>
2024-10-23 11:13:48 +0530

    Created spring-boot project skeleton code.

---
+++ freelancer/src/test/java/com/tadaah/freelancer/FreelancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadaah.freelancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
